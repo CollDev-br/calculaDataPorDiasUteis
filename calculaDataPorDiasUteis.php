d3d056fbb9e44d3a689d8bb0d8ed3f2f796b9503
Lucas Almeida <84864319+lkameida@users.noreply.github.com>
2021-05-26 14:36:36 -0300

    init

--- calculaDataPorDiasUteis.php
+++ calculaDataPorDiasUteis.php
@@ -7,12 +7,12 @@ function diasUteis($prazo)
     $a = 1; // Instancia a variável $a para o while
 
     while ($a < $prazo) {
-        $data_passe = date("Y-m-d", strtotime($data_passe . '+ 1 days')); // 
-        if (date('w', strtotime($data_passe)) > 0 && date('w', strtotime($data_passe)) < 6) {
-            $data_calc = date("Y-m-d", strtotime($data_passe . '+ 1 days'));
-            ++$a;
+        $data_passe = date("Y-m-d", strtotime($data_passe . '+ 1 days')); // data passe fica responsável por ir contando os dias independente de ser útil ou não
+        if (date('w', strtotime($data_passe)) > 0 && date('w', strtotime($data_passe)) < 6) { // Agora ele verifica se a data_passe é dia de semana 
+            $data_calc = date("Y-m-d", strtotime($data_passe . '+ 1 days')); // se for ele soma um dia na data_calc que é o nosso valor final
+            ++$a; // acrescenta a volta no loop
         }
     }
 
-    return $data_calc;
+    return $data_calc; // retorna o valor para seu código
 }
